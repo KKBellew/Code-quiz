3c4c3270d658cc3e2ee42e8869f98c197011ece8
kkbellew <kkbellew@gmail.com>
2023-10-16 12:26:03 -0500

    added to html script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const questoins= [
+const questions= [
     {
         question:"Commonly used data types DO NOT include?",
         answers:[ 
@@ -46,6 +46,85 @@ const questoins= [
     },
 ];
 const questoinElement = docement.getElementById("questoin");
-const answerButton = docement.getElementById("answer-button");
-
+const answerButtons = docement.getElementById("answer-button");
+const nextButton = docement.getElementById("next-btn");
  
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHtML = "Next"
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex]
+    let questionNo = currentQuestionIndex = + 1;
+    questoinElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+    if(answer.correct){
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener(click, selectAnswer);
+});
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dadaset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.class.add("incorrect");
+
+    }
+    Array.form(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct ==="true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = "block";
+}
+function showScore(){
+    resetState();
+    questoinElement.innerHTML = "You scored ${score} out of ${questions.length}."; 
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display = "block"
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex <question.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+startQuiz( )
