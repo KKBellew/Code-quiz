e727b317c486be82f607d47bb00ff1466e8449d3
kkbellew <kkbellew@gmail.com>
2023-10-16 10:18:58 -0500

    added questoins js and css

---
+++ script.js
@@ -0,0 +1,51 @@
+const questoins= [
+    {
+        question:"Commonly used data types DO NOT include?",
+        answers:[ 
+            { text: "Alerts", correct: true},
+            { text: "Boolean", correct: false},
+            { text: "Strings", correct: false},
+            { text: "Numbers", correct: false},
+    ]
+    },
+    {   question:"The condition in an if/else statement is enclosed within _____.",
+        answers:[ 
+            { text: "Quotes", correct: false},
+            { text: "Curly brackets", correct: false},
+            { text: "Parentheses", correct: true},
+            { text: "Square brackets", correct: false},
+        ]
+
+    },
+    {   question:"Arrays in JavaScript can be used to store _____.",
+        answers:[ 
+            { text: "Nuymbers and strings", correct: false},
+            { text: "Other arrays", correct: false},
+            { text: "Booleans", correct: false},
+            { text: "All of the above", correct: true},
+        ]
+
+    },
+    {   question:"What does HTML stand for?",
+        answers:[ 
+            { text: "Hypertext Markup Language", correct: true},
+            { text: "Hypertext Model Language", correct: false},
+            { text: "Hyper Modulus Language", correct: false},
+            { text: "None of the above", correct: false},
+        ]
+
+    },
+    {   question:"String values must be enclosed within ____ when being assigned to variables.",
+        answers:[ 
+            { text: "Commas", correct: false},
+            { text: "Quotation marks", correct: true},
+            { text: "Curly brackets", correct: false},
+            { text: "Parenetheses", correct: false},
+        ]
+
+    },
+];
+const questoinElement = docement.getElementById("questoin");
+const answerButton = docement.getElementById("answer-button");
+
+ 
